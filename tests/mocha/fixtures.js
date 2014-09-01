c913dc009cd9b01f6313b921c2b1501b84033c69
David Herges <david@spektrakel.de>
2014-09-01 21:29:28 +0200

    Test for the fixtures run through `grunt bower`

---
+++ tests/mocha/fixtures.js
@@ -0,0 +1,40 @@
+/*
+ * grunt-bower
+ * https://github.com/dherges/grunt-bower
+ *
+ * Copyright (c) 2014 David Herges
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var assert = require('chai').assert
+var fs = require('fs')
+
+describe('fixtures for "grunt bower"', function () {
+
+  it('must install bootstrap', function (done) {
+    var path = 'tests/output/bootstrap/.bower.json'
+    fs.exists(path, function (exists) {
+      assert.ok(exists, path + ' must exist!')
+      done()
+    })
+  })
+
+  it('must install jquery', function (done) {
+    var path = 'tests/output/jquery/.bower.json'
+    fs.exists(path, function (exists) {
+      assert.ok(exists, path + ' must exist!')
+      done()
+    })
+  })
+
+  it('must install typeahead.js', function (done) {
+    var path = 'tests/output/typeahead.js/.bower.json'
+    fs.exists(path, function (exists) {
+      assert.ok(exists, path + ' must exist!')
+      done()
+    })
+  })
+
+})
